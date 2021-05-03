b37f70ea58071bdd1142ac842b118e199ad08b2b
ag-vectivus <a.s.galecki@gmail.com>
2021-05-03 16:22:24 +0200

    Set up forecast script - get data from API

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,1 +1,25 @@
 "use strict";
+
+const key = "[API KEY]";
+
+// get weather information
+const getWeather = async (id) => {
+	const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+	const query = `${id}?apikey=${key}`;
+
+	const response = await fetch(base + query);
+	const data = await response.json();
+
+	return data[0];
+};
+
+// get city information
+const getCity = async (city) => {
+	const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+	const query = `?apikey=${key}&q=${city}`;
+
+	const response = await fetch(base + query);
+	const data = await response.json();
+
+	return data[0];
+};
